0a4021778cf834700eb683ea9b7369643378efbf
Rockey <41846794+RockeyCoss@users.noreply.github.com>
2021-03-11 23:44:32 +0800

    Add files via upload

--- models/KNN.py
+++ models/KNN.py
@@ -15,7 +15,7 @@ class KNN(ModelBaseClass):
 
 
 class Node:
-    def __init__(self, points: list = None, father=None, lChild=None, rChild=None, axis: int = None):
+    def __init__(self, points: np.array = None, father=None, lChild=None, rChild=None, axis: int = None):
         self.points = points
         self.father = father
         self.lChild = lChild
@@ -27,27 +27,44 @@ class kdTree:
     def __init__(self, root=None):
         self.root = root
 
-    def medianSplit(self, data, axis):
-        sortedData = np.array(sorted(data, key=lambda x: x[axis]))
+    def medianSplit(self, features, axis,axisNum):
+        if features.shape[0]==1:
+            return None,features,None
+        sortedData = np.array(sorted(features, key=lambda x: x[axis]))
         medianIndex = sortedData.shape[0] // 2
         leftSame, rightSame = True
         leftStep = 1
         rightStep = 1
         medianValue = sortedData[medianIndex, axis]
+
         while leftSame or rightSame:
-            if sortedData[medianIndex - leftStep, axis] < medianValue:
+            if medianIndex-leftStep<0 or sortedData[medianIndex - leftStep, axis] < medianValue:
                 leftSame = False
             else:
                 leftStep += 1
 
-            if sortedData[medianIndex + rightStep, axis] > medianValue:
+            if medianIndex+rightStep>=sortedData.shape[0] or sortedData[medianIndex + rightStep, axis] > medianValue:
                 rightSame = False
             else:
                 rightStep += 1
 
-        return sortedData[:medianIndex - leftStep + 1], sortedData[
-                                                        medianIndex - leftStep + 1:medianIndex + rightStep], sortedData[
-                                                                                                             medianIndex + rightStep:]
+        leftData=sortedData[:medianIndex - leftStep + 1]
+        medianData=sortedData[medianIndex - leftStep + 1:medianIndex + rightStep]
+        rightData=sortedData[medianIndex + rightStep:]
+
+        if leftData.shape[0]==0:
+            leftData=None
+        if rightData.shape[0]==0:
+            rightData=None
+
+        return leftData,medianData,rightData
 
     def createKdTree(self, features: np.array):
         self.root = Node(axis=0)
+
+
+    def createChild(self,features:np.array,currentNode:Node,depth:int,axisNum:int):
+        axis=depth%axisNum
+        currentNode.axis=axis
+        leftData,medianData,rightData=self.medianSplit(features,axis=axis)
+        currentNode.points=medianData.reshape(-1,axisNum)

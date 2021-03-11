9a04a1e7cf63b611a4bbb8a55eb95933fe4113bc
Rockey <41846794+RockeyCoss@users.noreply.github.com>
2021-03-12 00:06:36 +0800

    Finished the codes for creating kd-tree

--- models/KNN.py
+++ models/KNN.py
@@ -27,9 +27,9 @@ class kdTree:
     def __init__(self, root=None):
         self.root = root
 
-    def medianSplit(self, features, axis,axisNum):
-        if features.shape[0]==1:
-            return None,features,None
+    def medianSplit(self, features: np.ndarray, axis: int):
+        if features.shape[0] == 1:
+            return None, features, None
         sortedData = np.array(sorted(features, key=lambda x: x[axis]))
         medianIndex = sortedData.shape[0] // 2
         leftSame, rightSame = True
@@ -38,33 +38,49 @@ class kdTree:
         medianValue = sortedData[medianIndex, axis]
 
         while leftSame or rightSame:
-            if medianIndex-leftStep<0 or sortedData[medianIndex - leftStep, axis] < medianValue:
+            if medianIndex - leftStep < 0 or sortedData[medianIndex - leftStep, axis] < medianValue:
                 leftSame = False
             else:
                 leftStep += 1
 
-            if medianIndex+rightStep>=sortedData.shape[0] or sortedData[medianIndex + rightStep, axis] > medianValue:
+            if medianIndex + rightStep >= sortedData.shape[0] or sortedData[
+                medianIndex + rightStep, axis] > medianValue:
                 rightSame = False
             else:
                 rightStep += 1
 
-        leftData=sortedData[:medianIndex - leftStep + 1]
-        medianData=sortedData[medianIndex - leftStep + 1:medianIndex + rightStep]
-        rightData=sortedData[medianIndex + rightStep:]
+        leftData = sortedData[:medianIndex - leftStep + 1]
+        medianData = sortedData[medianIndex - leftStep + 1:medianIndex + rightStep]
+        rightData = sortedData[medianIndex + rightStep:]
 
-        if leftData.shape[0]==0:
-            leftData=None
-        if rightData.shape[0]==0:
-            rightData=None
+        if leftData.shape[0] == 0:
+            leftData = None
+        if rightData.shape[0] == 0:
+            rightData = None
 
-        return leftData,medianData,rightData
+        return leftData, medianData, rightData
 
-    def createKdTree(self, features: np.array):
-        self.root = Node(axis=0)
+    def createKdTree(self, features: np.ndarray):
+        self.root = Node()
+        assert type(features) == np.ndarray and (features.shape) == 2
+        self.createChild(features, self.root, 0)
 
+    def createChild(self, features: np.ndarray, currentNode: Node, depth: int):
+        axis = depth % features.shape[1]
+        currentNode.axis = axis
+        leftData, medianData, rightData = self.medianSplit(features, axis=axis)
+        assert len(medianData.shape) == 2
+        currentNode.points = medianData
+        if leftData == None:
+            currentNode.lChild = None
+        else:
+            currentNode.lChild = Node(father=currentNode)
+            self.createChild(leftData, currentNode.lChild, depth + 1)
 
-    def createChild(self,features:np.array,currentNode:Node,depth:int,axisNum:int):
-        axis=depth%axisNum
-        currentNode.axis=axis
-        leftData,medianData,rightData=self.medianSplit(features,axis=axis)
-        currentNode.points=medianData.reshape(-1,axisNum)
+        if rightData == None:
+            currentNode.rChild = None
+        else:
+            currentNode.rChild = Node(father=currentNode)
+            self.createChild(rightData, currentNode.rChild, depth + 1)
+
+        return

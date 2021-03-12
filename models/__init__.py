29a57bb8a03623dd0eea5ade3dbf2dcec2a6df79
Rockey <41846794+RockeyCoss@users.noreply.github.com>
2021-03-12 21:56:37 +0800

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from models.Perceptron import Perceptron
 from models.NaiveBayesian import NaiveBayesian
 from models.Logistic import Logistic
 from models.MaximumEntropy import MaximumEntropy
+from models.KNN import KNN

2479697f4443de4390551c68421a23b13656e5e9
Rockey <41846794+RockeyCoss@users.noreply.github.com>
2021-03-16 00:17:32 +0800

    temp save

--- models/SVM.py
+++ models/SVM.py
@@ -3,19 +3,41 @@ import json
 import sklearn
 from models import ModelBaseClass
 
+
 class SVM(ModelBaseClass):
     def __init__(self):
-        self.featureNum=0
-    def rbf(self,x1,x2):
-        return np.exp(-np.power((x1-x2),2))/self.featureNum
-    def getKernelMatrix(self,kernel)->np.ndarray:
-        pass
+        self.rbfGamma = 0
+        self.sampleNum = 0
+        self.C = 1
+
+    def rbf(self, x1, x2):
+        """
+        以sklearn “auto” 方式确定gamma
+        """
+        return np.exp(-np.sum(np.power((x1 - x2), 2)) / self.rbfGamma)
+
+    def getKernelMatrix(self, features: np.ndarray, kernel) -> np.ndarray:
+        """
+        计算K(xi,xj)矩阵
+        :param features: 测试样例
+        :param kernel: 核函数
+        :return: K(xi,xj)矩阵。考虑到训练集中样例较少，因此matrix暂不用cache方式而是整体储存
+        """
+        kernelMatrix = np.zeros((self.sampleNum, self.sampleNum))
+        for i in range(self.sampleNum):
+            for j in range(i + 1):
+                kernelMatrix[i, j] = kernelMatrix[j, i] = kernel(features[i], features[j])
+        return kernelMatrix
+
     def train(self, features: np.ndarray, labels: np.ndarray, *args, **dicts):
-        self.featureNum=features.shape[1]
+        self.rbfGamma = features.shape[1] if features.shape[1] > 0 else 1
+        self.sampleNum = features.shape[0]
+        self.C = dicts['C'] if 'C' in dicts.keys() else 1
 
     def predict(self, features: np.array):
         super().predict(features)
 
         sklearn.svm()
+
     def loadPara(self):
         super().loadPara()

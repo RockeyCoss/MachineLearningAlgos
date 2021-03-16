9d26c2d082bcfe9bb6a3f3a7e31680ee8673241d
Rockey <41846794+RockeyCoss@users.noreply.github.com>
2021-03-16 23:53:37 +0800

    SVM over 50%

--- models/SVM.py
+++ models/SVM.py
@@ -6,7 +6,7 @@ from models import ModelBaseClass
 # paper Sequential Minimal Optimization:A Fast Algorithm for Training Support Vector Machines
 # optimized the program according to a zhihu answer:
 # https://www.zhihu.com/question/31211585/answer/640501555?utm_source=qq&utm_medium=social&utm_oi=710101447663972352
-
+# non-bound examples: examples with alphas that are neither 0 nor C
 class SVM(ModelBaseClass):
     def __init__(self):
         self.rbfGamma:float = 0
@@ -47,21 +47,16 @@ class SVM(ModelBaseClass):
         self.features=features
         self.labels=labels
         self.kernelMatrix=self.getKernelMatrix()
+        #hot data
         self.alphaNot0NorC=[]
-        # for every non-bound example, error cache stores it's Ei
-        # for other examples, error cache stores
-        # 两种方案，这里采取了空间换时间的方案
-        # 省空间方案：在errorCache里仅存（index,Ei),并且errorCache按index排序
-        # 这样插入，删除和修改都要logn，但是空间能节省（前期没节省多少，后期还是可的）
-        # 省时间方案：errorCache和sampleNum登长，0表示非non-bound example(non-bound example不可能有g(xi)-yi=0)，
-        # 这里采用省时间方案
-        self.errorCache=(np.sum((self.alhpa*self.labels*self.kernelMatrix),axis=1)+self.b)-self.labels
-        
+        #the actural time comsuming computation
+        # Ei=wx[i]+b-yi
+        self.wx=np.zeros(self.sampleNum)
+
+
 
     def predict(self, features: np.array):
         super().predict(features)
-
-        sklearn.svm()
 
     def loadPara(self):
         super().loadPara()
@@ -73,17 +68,21 @@ class SVM(ModelBaseClass):
             changedNum=0
             if examineAll:
                 for sampleIndex in range(self.sampleNum):
-                    changedNum+=self.__exaime(sampleIndex)
+                    changedNum+=self.__examime(sampleIndex)
             else:
                 for listIndex,sampleIndex in enumerate(self.alphaNot0NorC):
                     changedNum+=self.__examine(sampleIndex, listIndex)
 
             examineAll=False if examineAll else True
 
+    def __getI1Heuristically(self,E2):
+        if E2>=0:
+            return np.argmax(self.wx+self.b-self.labels)
+
     def __examine(self, sampleIndex:int, listIndex:int=-1)->int:
         y2=self.labels[sampleIndex]
         alpha2=self.alhpa[sampleIndex]
-        E2=self.errorCache[sampleIndex]
+        E2=self.wx[sampleIndex]+self.b-self.labels[sampleIndex]
         r2=E2*y2
         # KKT condition;
         # alphaI==0<=>yi(W*Xi+b)>=1<=>yi(W*Xi+b-yi)>=0
@@ -92,7 +91,45 @@ class SVM(ModelBaseClass):
         # examine whether the KKT conditions are satisfied:
         if (r2<-self.epsilon and alpha2<self.C) or (r2>self.epsilon and alpha2>0):
             #not satisfied
+            alphaNot0NorClength=len(self.alphaNot0NorC)
+            if alphaNot0NorClength>1:
+                alpha1Index=-1
+                # because of the list of alphaNot0NorC, the program doesn't need to spend time computing
+                # bound examples' E
+                if E2>=0:
+                    indexOfAlpha1Index=np.argmax((self.wx[self.alphaNot0NorC]+self.b-self.labels[self.alphaNot0NorC]))
+                else:
+                    indexOfAlpha1Index=np.argmin((self.wx[self.alphaNot0NorC]+self.b-self.labels[self.alphaNot0NorC]))
 
+                alpha1Index = self.alphaNot0NorC[indexOfAlpha1Index]
+                assert alpha1Index!=-1
+
+                if self.__optimize(alpha1Index,sampleIndex):
+                    return 1
+
+            #SMO can't make positive progress using alpha1Index
+            start=np.random.randint(low=0,high=alphaNot0NorClength)
+            for delta in range(0,alphaNot0NorClength):
+                indexOfAnotherIndex=(start+delta)%alphaNot0NorClength
+                if indexOfAnotherIndex==indexOfAlpha1Index:
+                    continue
+                anotherIndex=self.alphaNot0NorC[indexOfAnotherIndex]
+                if self.__optimize(anotherIndex,sampleIndex):
+                    return 1
+
+            # desperately search
+            boundExamples=list(set(i for i in range(self.sampleNum))-set(self.alphaNot0NorC))
+            start=np.random.randint(low=0,high=len(boundExamples))
+            for delta in range(0,len(boundExamples)):
+                indexOfDesperateIndex=(start+delta)%len(boundExamples)
+                desperateIndex=boundExamples[indexOfDesperateIndex]
+                if self.__optimize(desperateIndex,sampleIndex):
+                    return 1
+            # give up optimizing sampleIndex
+            return 0
         else:
             #satisfied
             return 0
+
+        def __optimize(self,alpha1Index:int,alpha2Index:int)->bool:
+            return False

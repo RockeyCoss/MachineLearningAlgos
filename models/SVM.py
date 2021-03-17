66675a384a6bf8ac9699ff9a121acac80eaf5947
Rockey <41846794+RockeyCoss@users.noreply.github.com>
2021-03-17 21:07:07 +0800

    modify SVM

--- models/SVM.py
+++ models/SVM.py
@@ -1,19 +1,21 @@
 import numpy as np
-import json
-import sklearn
 from models import ModelBaseClass
-
+from utilities import loadConfigWithName
 
 # refer to Statistical Learning Methods and
 # paper Sequential Minimal Optimization:A Fast Algorithm for Training Support Vector Machines
 # optimized the program according to a zhihu answer:
 # https://www.zhihu.com/question/31211585/answer/640501555?utm_source=qq&utm_medium=social&utm_oi=710101447663972352
 # non-bound examples: examples with alphas that are neither 0 nor C
+
+
+
 class SVM(ModelBaseClass):
     def __init__(self):
         self.rbfGamma: float = 0
         self.sampleNum: int = 0
-        self.C: float = 1
+        self.C: float = float(loadConfigWithName("SVMConfig", "C"))
+        self.rbfGamma:float=0
         self.epsilon: float = 1e-3
         self.alpha: np.ndarray = None
         self.b: float = None
@@ -45,14 +47,17 @@ class SVM(ModelBaseClass):
         return kernelMatrix
 
     def train(self, features: np.ndarray, labels: np.ndarray, *args, **dicts):
-        self.rbfGamma = features.shape[1] if features.shape[1] > 0 else 1
+        gamma = loadConfigWithName("SVMConfig", "gamma")
+        if gamma == "auto":
+            self.rbfGamma = features.shape[1] if features.shape[1] > 0 else 1
+        else:
+            self.rbfGamma=float(gamma)
         self.sampleNum = features.shape[0]
-        self.C = dicts['C'] if 'C' in dicts.keys() else 1
         self.alpha = np.zeros(self.sampleNum)
         self.b = 0
         self.features = features
         self.labels = labels
-        self.kernelMatrix = self.__getKernelMatrix()
+        self.kernelMatrix = self.__getKernelMatrix(self.rbf)
         # hot data
         self.alphaNot0NorC = []
         # the actual time consuming computation
@@ -89,10 +94,10 @@ class SVM(ModelBaseClass):
             changedNum = 0
             if examineAll:
                 for sampleIndex in range(self.sampleNum):
-                    changedNum += self.__examime(sampleIndex)
+                    changedNum += self.__examine(sampleIndex)
             else:
-                for listIndex, sampleIndex in enumerate(self.alphaNot0NorC):
-                    changedNum += self.__examine(sampleIndex, listIndex)
+                for sampleIndex in self.alphaNot0NorC:
+                    changedNum += self.__examine(sampleIndex)
 
             examineAll = False if examineAll else True
 
@@ -100,7 +105,7 @@ class SVM(ModelBaseClass):
         if E2 >= 0:
             return np.argmax(self.wx + self.b - self.labels)
 
-    def __examine(self, sampleIndex: int, listIndex: int = -1) -> int:
+    def __examine(self, sampleIndex: int) -> int:
         y2 = self.labels[sampleIndex]
         alpha2 = self.alpha[sampleIndex]
         E2 = self.wx[sampleIndex] + self.b - self.labels[sampleIndex]
@@ -131,14 +136,15 @@ class SVM(ModelBaseClass):
                     return 1
 
             # SMO can't make positive progress using alpha1Index
-            start = np.random.randint(low=0, high=alphaNot0NorClength)
-            for delta in range(0, alphaNot0NorClength):
-                indexOfAnotherIndex = (start + delta) % alphaNot0NorClength
-                if indexOfAnotherIndex == indexOfAlpha1Index:
-                    continue
-                anotherIndex = self.alphaNot0NorC[indexOfAnotherIndex]
-                if self.__optimize(anotherIndex, sampleIndex):
-                    return 1
+            if alphaNot0NorClength>0:
+                start = np.random.randint(low=0, high=alphaNot0NorClength)
+                for delta in range(0, alphaNot0NorClength):
+                    indexOfAnotherIndex = (start + delta) % alphaNot0NorClength
+                    if indexOfAnotherIndex == indexOfAlpha1Index:
+                        continue
+                    anotherIndex = self.alphaNot0NorC[indexOfAnotherIndex]
+                    if self.__optimize(anotherIndex, sampleIndex):
+                        return 1
 
             # desperately search
             boundExamples = list(set(i for i in range(self.sampleNum)) - set(self.alphaNot0NorC))
@@ -214,10 +220,14 @@ class SVM(ModelBaseClass):
         b1 = E1 + y1 * (a1 - alpha1) * K11 + y2 * (a2 - alpha2) * K12 + self.b
         b2 = E2 + y1 * (a1 - alpha1) * K11 + y2 * (a2 - alpha2) * K22 + self.b
         if 0 < a1 < self.C:
-            self.alphaNot0NorC.append(alpha1Index)
+            tempSet=set(self.alphaNot0NorC)
+            tempSet.add(alpha1Index)
+            self.alphaNot0NorC = list(tempSet)
             self.b = b1
         if 0 < a2 < self.C:
-            self.alphaNot0NorC.append(alpha2Index)
+            tempSet = set(self.alphaNot0NorC)
+            tempSet.add(alpha2Index)
+            self.alphaNot0NorC=list(tempSet)
             self.b = b2
         else:
             self.b = (b1 + b2) / 2
--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,4 @@ from models.NaiveBayesian import NaiveBayesian
 from models.Logistic import Logistic
 from models.MaximumEntropy import MaximumEntropy
 from models.KNN import KNN
+from models.SVM import SVM
